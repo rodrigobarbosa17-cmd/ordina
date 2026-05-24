f0ef59c59fab4257e6a911f5eee02ae0421103e8
Rodrigo Barbosa <rodrigo.silveira2@aluno.ce.gov.br>
2026-05-24 13:40:23 -0300

    adiciona requirements e ajusta porta

--- app.py
+++ app.py
@@ -691,4 +691,4 @@ if __name__ == '__main__':
     init_db()
     print("\n🧹 Ordina — Sistema iniciado!")
     print("📌 Acesse: http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
